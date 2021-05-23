5a429c938479b17b64bf7767625fbfcf08c6db36
denven <myregs@foxmail.com>
2021-05-22 22:18:47 -0700

    Added box shadow for test button

--- src/components/TestSound.js
+++ src/components/TestSound.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const TestSound = () => {
   return (
-    <div className='fixed h-14 inset-x-0 bottom-4 mx-8 border font-medium border-white rounded-md flex justify-center items-center' style={{backgroundColor: '#FBED96'}}>
+    <div className='fixed h-14 inset-x-0 bottom-4 mx-8 border font-medium border-white rounded-md flex justify-center items-center' 
+      style={{backgroundColor: '#FBED96', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}
+    >
       Test your surounding
     </div>
   )
