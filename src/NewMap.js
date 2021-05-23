5d61c5da5df3b636f6ad512704b6eb1dc0147090
kennethzhang <kennethzhang374@gmail.com>
2021-05-23 01:22:56 -0700

    new map works with multiple nodes on map

--- src/NewMap.js
+++ src/NewMap.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { useGlobalContext } from './context'
+import AutoComplete from 'react-google-autocomplete'
 const fetch = require('isomorphic-fetch')
 const { compose, withProps, withHandlers } = require('recompose')
 const { withScriptjs, withGoogleMap, GoogleMap, Marker } = require('react-google-maps')
@@ -22,7 +24,13 @@ const MapWithAMarkerClusterer = compose(
   withScriptjs,
   withGoogleMap,
 )((props) => (
-  <GoogleMap defaultZoom={12} defaultCenter={{ lat: 25.0391667, lng: 121.525 }}>
+  <GoogleMap defaultZoom={12} defaultCenter={{ lat: 49.16659, lng: -123.133569 }}>
+    <AutoComplete
+      className={'border-2 border-yellow-500 block w-full p-2 mt-2 rounded md:w-1/3 m-auto'}
+      onPlaceSelected={(place) => {
+        console.log(place)
+      }}
+    />
     <MarkerClusterer onClick={props.onMarkerClustererClick} averageCenter enableRetinaIcons gridSize={60}>
       {props.markers.map((marker) => (
         <Marker key={marker.photo_id} position={{ lat: marker.latitude, lng: marker.longitude }} />
