9284d9e2b090ef0b4a309d5dd7328337c7289df8
kennethzhang <kennethzhang374@gmail.com>
2021-05-22 17:14:57 -0700

    add react-google-map, google-autocomplete, and geocode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Map from './Map';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <div className='h-screen flex flex-col justify-center items-center'>
+      <SearchBar />
       <Map />
     </div>
   );
--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,10 @@
 import React from 'react';
+// import { GoolgeMap } from 'react-google-maps';
 
 function Map() {
   return <div className='h-32 bg-green-500'>This is the map</div>;
 }
 
 export default Map;
+
+//geocoding api key = AIzaSyAQB5MRsLFxZyDtCMWlwd8WSSsUi0B17Wo
